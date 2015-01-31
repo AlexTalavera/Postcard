cbd23d22d1d3ba4bc9cb3bfc9a8fcacd30bfc039
Alejandro talavera <Alex@iMac-de-Alejandro.local>
2015-01-31 00:56:21 -0600

    first rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var messageSent: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder() //funcion para ocultar el teclado
+        
+        messageSent.setTitle("Mail Sent", forState: UIControlState.Normal) //cambia el texto que hay en el boton, el primer parametro recibe un string y el segundo indica el tipo de texto que se mostrara (subrayado, normal etc.)
+        
+        
+        
+    }
 
 }
 
+ 
